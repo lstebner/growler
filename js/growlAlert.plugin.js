6dfaa25dab7def3e26b963557b4ecc4d194ed170
lstebner <luke.stebner@gmail.com>
2011-02-21 22:37:27 -0800

    custom icon; 4 corner positioning support; onShow callback added; onComplete callback added; readme updated

--- js/growlAlert.plugin.js
+++ js/growlAlert.plugin.js
@@ -1,32 +1,84 @@
 (function( $ ){
 	$.fn.growlAlert = function(opts, data){
-		$self = this;
-		
-		//settings for growl
-		settings = {
+	    //settings for growl
+		var settings = {
 			autoShow : true,
 			fadeTime : 600,
-			delayTime : 5000,
+			delayTime : 1500,
 			title : 'Growl Alert Title',
-			content : 'This is the content for the growl alert test message, woo!'
+			content : 'This is the content for the growl alert test message, woo!',
+			closeIcon : 'http://www.linnnk.com/demos/images/close-icon.png',
+			onShow: null,
+			onComplete: null,
+			position: 'top-left'
 		};
 		
-		//HTML to insert
-		growlHTML = '<a href="#" class="close-link"><img src="http://www.linnnk.com/demos/images/close-icon.png" width="20px" height="20px" /></a><div class="title"></div><div class="content"></div>';
+	    if (this.html() == ''){
+    		this.html('<a href="#" class="close-link"><img src="' + settings.closeIcon + '" width="20px" height="20px" /></a><div class="title"></div><div class="content"></div>');
+	    }
+	    
+		$self = this;
+        $close = this.find('.close-link');
+		$title = this.children('.title');
+		$content = this.children('.content');
 		
 		//methods for growl
 		methods = {
+		    set: function(key, val){
+		        switch(key){
+		            case 'title':
+		                $title.text(val);
+		                break;
+		            
+		            case 'content':
+		                $content.text(val);
+		                break;
+		                
+		            case 'position':
+		                $self.removeClass('top-left').removeClass('top-right').removeClass('bottom-left').removeClass('bottom-right').addClass(val);
+		                break;
+		        }
+		    },
+		    onComplete: function(callback){
+		        if (settings.onComplete){
+		            settings.onComplete();
+		        }
+		    },
+		    onShow: function(){
+		        if (settings.onShow){
+		            settings.onShow();
+		        }
+		    },
 			show : function(data){
-			    if (data){
-			        if (data.title){
-			            $self.children('.title').text(data.title);
-			        }
-			        if (data.content){
-			            $self.children('.content').text(data.content);
+			    if ($self.is(':visible')){
+			        old = methods.onComplete();
+			        methods.onComplete = function(){
+                        if (old){
+                            old();
+                        }
+                        
+                        $self.growlAlert('show', data);
 			        }
 			    }
-			    
-				return $self.fadeIn(settings.fadeTime).delay(settings.delayTime).fadeOut(settings.fadeTime);
+			    else{
+    			    if (data){
+    			        if (data.title){
+                            methods.set('title', data.title);
+    			        }
+    			        if (data.content){
+                            methods.set('content', data.content);
+    			        }
+    			        if (data.position){
+    			            methods.set('position', data.position);
+    		            }
+		        
+    			        $.extend(settings, data);
+    			    }
+		    
+    				return $self.fadeIn(settings.fadeTime, function(){ methods.onShow(); })
+    				            .delay(settings.delayTime)
+    				            .fadeOut(settings.fadeTime, function(){ methods.onComplete(); });
+    			}
 			}
 		};
 		
@@ -42,27 +94,23 @@
 			}
 		}
 		
-		//insert the HTML if none is there
-		if (this.html() == ''){
-			this.html(growlHTML);
+		if (!this.hasClass(settings.position)){
+		    methods.set('position', settings.position);
 		}
 		
 		//objects
 		this.hide();
-		$close = this.find('.close-link');
-		$title = this.children('.title');
-		$content = this.children('.content');
 		
 		//set the text
-		$title.text(settings.title);
-		$content.text(settings.content);
+		methods.set('title', settings.title);
+		methods.set('content', settings.content);
 		
 		//close button click
 		$close.click(function(){
 			//stop any animations running now
 			$self.stop(true, true);
 			//fade out the dialog
-			$self.fadeOut(settings.fadeTime);
+			$self.fadeOut(settings.fadeTime, function(){ methods.onComplete(); });
 			//prevent default action on the close link
 			return false;
 		});
