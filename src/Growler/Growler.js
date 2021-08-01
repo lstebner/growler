b662d3eedde5382bbec6b7ddc981f83631cde74f
Luke Stebner <luke.stebner@gmail.com>
2021-08-01 15:04:19 -0700

    fixup! refactor: complete rewrite using vanilla javascripts

--- src/Growler/Growler.js
+++ src/Growler/Growler.js
@@ -1,13 +1,14 @@
 import styles from "./Growler.styles.css"
 
-export default function Growler(options = {}) {
+export default function Growler(options) {
   const mergedOptions = Object.assign({}, Growler.defaultOptions, options)
-  const { duration } = mergedOptions
+  const { duration, position } = mergedOptions
+  
   const growl = document.createElement("div")
   growl.className = styles.message
   growl.innerHTML = options.contents
 
-  const messageContainerId = `growler__messages-container__${mergedOptions.position}`
+  const messageContainerId = `growler__messages-container__${position}`
 
   let messagesContainer = document.getElementById(messageContainerId)
 
@@ -15,7 +16,7 @@ export default function Growler(options = {}) {
     messagesContainer = document.createElement("div")
     messagesContainer.id = messageContainerId 
     messagesContainer.className = styles.messagesContainer
-    messagesContainer.classList.add(styles[`position${mergedOptions.position}`])
+    messagesContainer.classList.add(styles[`position${position}`])
 
     document.body.appendChild(messagesContainer)
   }
@@ -28,7 +29,6 @@ export default function Growler(options = {}) {
 }
 
 Growler.defaultOptions = {
-  autoHide: true,
   duration: 3000,
   contents: "",
   position: "TopRight",
