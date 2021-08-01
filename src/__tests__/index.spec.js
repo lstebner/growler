604bd2d1c762458a6101d4895f5b52bde64455e8
Luke Stebner <luke.stebner@gmail.com>
2021-08-01 15:03:23 -0700

    fixup for configuartion issues (corejs and css for jest)

--- src/__tests__/index.spec.js
+++
@@ -1,9 +0,0 @@
-import Growler from ".."
-import { getByText } from "@testing-library/dom"
-
-describe("Growler", () => {
-  it("can display message", () => {
-    Growler({ contents: "hello world" })
-    expect(getByText(document.body, "hello world")).toBeInTheDocument()
-  })
-})
